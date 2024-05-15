67cecbb9c4e35d29f0995e3fc1e4eafd12313ab6
BrenoCode <breno.cavalcante@ufu.br>
2024-05-15 16:24:10 -0300

    insert implementation

--- demo_dao_jdbc/src/application/Program.java
+++ demo_dao_jdbc/src/application/Program.java
@@ -1,9 +1,11 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
@@ -26,6 +28,12 @@ public class Program {
 		for(Seller s : list) {
 			System.out.println(s);
 		}
+		
+		System.out.println("\n=== TESTE 4: seller Insert ===");
+		Department dep = new Department(2,null);
+		Seller newSeller = new Seller(null, "Greg", "Greg@docris.com", new Date(), 4000.0, dep);
+		sl.insert(newSeller);
+		System.out.println(newSeller.getId());
 		
 	}
 
