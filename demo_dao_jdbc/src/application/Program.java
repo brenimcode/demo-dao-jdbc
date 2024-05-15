fda6c83e0bbf01c89955a332ed51825c90c9fb54
BrenoCode <breno.cavalcante@ufu.br>
2024-05-15 15:50:51 -0300

    findById

--- demo_dao_jdbc/src/application/Program.java
+++ demo_dao_jdbc/src/application/Program.java
@@ -2,16 +2,19 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
-		Department dp = new Department(1,"Books");
-		System.out.println(dp);
-		Seller sl = new Seller(21,"bob","B@gmail",new Date(), 3000.0, dp);
-		System.out.println(sl);
+		SellerDao sl =  DaoFactory.createSellerDao();
+		
+		Seller seller = sl.findById(3);
+		
+		System.out.println(seller);
 		
 	}
 
